944709bba9a18a2221e38f2ada6ca60172dda2ff
LeoSanchez1996 <121622384+LeoSanchez1996@users.noreply.github.com>
2023-01-17 10:48:48 -0300

    Update servicio-proyectos.service.ts

--- src/app/servicios/servicio-proyectos.service.ts
+++ src/app/servicios/servicio-proyectos.service.ts
@@ -8,18 +8,18 @@ import { proyectos } from '../modelo/proyectos';
 export class ServicioProyectosService {
 
   constructor(private http:HttpClient) {}
-    Url='https://backend-contenido.onrender.com/users/proyectos';
+    Url='https://miprimeraapi.onrender.com/api/auth/users/proyectos';
   
   getProyectos(){
     return this.http.get<proyectos[]>(this.Url)
   }
   postActualizarProy(datos:any, datosID:any){
-    return this.http.put<any>('https://backend-contenido.onrender.com/users/proyectos/'+datosID, datos);
+    return this.http.put<any>('https://miprimeraapi.onrender.com/api/auth/users/proyectos/'+datosID, datos);
   }
   postCrearProy(datos:any){
-    return this.http.post<any>('https://backend-contenido.onrender.com/users/proyectos', datos);
+    return this.http.post<any>('https://miprimeraapi.onrender.com/api/auth/users/proyectos', datos);
   }
   postBorrarProy(datosID:any){
-    return this.http.delete<any>('https://backend-contenido.onrender.com/users/proyectos/'+datosID);
+    return this.http.delete<any>('https://miprimeraapi.onrender.com/api/auth/users/proyectos/'+datosID);
   }
 }
